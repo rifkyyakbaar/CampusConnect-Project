b7d9960b2f5e3875d02fed9230f513d22441a333
rifkyyakbaar <rifkyakbar169@gmail.com>
2026-05-30 21:50:30 +0800

    Update

---
+++ app/src/main/java/com/example/campusconnect_project/ui/panitia/DashboardPanitiaActivity.kt
@@ -0,0 +1,21 @@
+package com.example.campusconnect_project.ui.panitia
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.campusconnect_project.R
+
+class DashboardPanitiaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_dashboard_panitia)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
