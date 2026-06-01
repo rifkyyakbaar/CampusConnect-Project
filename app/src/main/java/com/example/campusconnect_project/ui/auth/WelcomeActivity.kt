d3dadad109be3168a5775dd4309c39b3d8e44270
Yurian20 <yfathurfajar@gmail.com>
2026-06-01 20:16:09 +0800

    udah ke login dan ke home

--- app/src/main/java/com/example/campusconnect_project/ui/auth/WelcomeActivity.kt
+++ app/src/main/java/com/example/campusconnect_project/ui/auth/WelcomeActivity.kt
@@ -6,10 +6,38 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.campusconnect_project.R
+import android.content.Intent
+import android.widget.Button
+import android.widget.TextView
+import com.example.campusconnect_project.ui.mahasiswa.HomeMahasiswaActivity
+
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val btnGetStarted = findViewById<Button>(R.id.btnGetStarted)
+        val tvGoToLogin = findViewById<TextView>(R.id.tvGoToLogin)
+
+        btnGetStarted.setOnClickListener {
+
+            startActivity(
+                Intent(
+                    this,
+                    HomeMahasiswaActivity::class.java
+                )
+            )
+
+        }
+
+        tvGoToLogin.setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    LoginActivity::class.java
+                )
+            )
+        }
     }
 }
--- app/src/main/java/com/example/campusconnect_project/ui/mahasiswa/HomeMahasiswaActivity.kt
+++ app/src/main/java/com/example/campusconnect_project/ui/mahasiswa/HomeMahasiswaActivity.kt
@@ -1,21 +1,13 @@
 package com.example.campusconnect_project.ui.mahasiswa
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.campusconnect_project.R
 
 class HomeMahasiswaActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_home_mahasiswa)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
